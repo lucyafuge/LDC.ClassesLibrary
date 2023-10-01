e83461f6e70196aa932b3ba2ddaa0c0d7cc1a140
Чешихин Артем <a.cheshihin@websoft.ru>
2023-10-01 20:38:24 +0300

    Добавьте файлы проекта.

---
+++ Data/SignContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDC.ClassesLibrary.Data
+{
+    internal class SignContext : DbContext
+    {
+        private string ConnectionString { get; set; }
+        public DbSet<Sign> Signs => Set<Sign>();
+
+        public SignContext(string connectionString) : base(connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+    }
+}
